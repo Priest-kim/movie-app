de0e971326a5bb149b2e10bbab88e45cc50cb4f3
jaden <hyunbin.kim.3376@gmail.com>
2022-05-15 17:02:33 +0900

    header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -11,17 +11,72 @@
     {
       href: "/movie/tt4520988",
       name: "Movie",
+      path: "/movie/*",
     },
     {
       href: "/about",
       name: "About",
+      path: "/about",
     },
   ];
 </script>
 
 <style lang="scss">
+  header {
+    padding: 20px 40px;
+    background-color: $color--black-90;
+    position: sticky;
+    top: 0;
+    z-index: 9;
+    display: flex;
+    align-items: flex-end;
+
+    nav {
+      margin-left: 40px;
+      ul {
+        display: flex;
+
+        li {
+          margin-left: 10px;
+
+          &:first-child {
+            margin-left: 0;
+          }
+
+          a {
+            text-decoration: none;
+            font-size: 14px;
+            color: $color--white-50;
+            font-weight: 700;
+          }
+        }
+      }
+    }
+    .user {
+      width: 40px;
+      height: 40px;
+      padding: 6px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      background-color: $color--area;
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 40px;
+      margin: auto;
+      transition: 0.4s;
+      &:hover {
+        background-color: lighten($color--area, 20%);
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+
   header :global(a.active) {
-    color: red;
+    color: $color--primary !important;
   }
 </style>
 
@@ -29,13 +84,16 @@
   <Logo />
   <nav>
     <ul>
-      {#each menus as { href, name } (name)}
+      {#each menus as { href, name, path } (name)}
         <li>
-          <a use:link use:active="{href}" href="{href}">
+          <a use:link use:active="{path}" href="{href}">
             {name}
           </a>
         </li>
       {/each}
     </ul>
   </nav>
+  <div class="user">
+    <img src="/assets/svelte.png" alt="User" />
+  </div>
 </header>
