fd4670a554b23caab1af0508c6468777dd7b0641
jaden <hyunbin.kim.3376@gmail.com>
2022-05-16 19:39:13 +0900

    axios

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { writable, get } from "svelte/store";
+
+export const movies = writable([]);
 
 export const searchMovies = async (payload) => {
   const { title, type, year, number } = payload;
@@ -8,5 +11,23 @@ export const searchMovies = async (payload) => {
     `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}`
   );
 
-  console.log(res);
+  const { Search, totalResults } = res.data;
+
+  movies.set(Search);
+
+  const pageLength = Math.ceil(totalResults / number);
+
+  if (pageLength > 1) {
+    for (let page = 2; page <= pageLength; page++) {
+      if (page > number / 10) break;
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+      );
+      const { Search } = res.data;
+      movies.update(($movies) => {
+        $movies.push(...Search);
+        return $movies;
+      });
+    }
+  }
 };
