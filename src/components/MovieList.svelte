a6f9f50191154948e43cf66b44936b21474638a2
jaden <hyunbin.kim.3376@gmail.com>
2022-05-19 22:42:01 +0900

    refactoring

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { movies, loading } from "~/store/movie";
+  import { movies, loading, message } from "~/store/movie";
   import MovieCard from "~/components/MovieCard.svelte";
   import Loader from "~/components/Loader.svelte";
 </script>
@@ -11,18 +11,29 @@
     background-color: $color--area;
     border-radius: 4px;
     text-align: center;
+    &.no-result {
+      padding: 70px 0;
+    }
     .movies {
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
     }
+    .message {
+      color: $color--primary;
+      font-size: 20px;
+      text-align: center;
+    }
   }
 </style>
 
-<div class="movie-list">
+<div class="movie-list" class:no-result="{!$movies.lenght}">
   {#if $loading}
     <Loader />
   {/if}
+  <div class="message">
+    {$message}
+  </div>
   <div class="movies">
     {#each $movies as movie (movie.imdbID)}
       <MovieCard movie="{movie}" />
--- src/store/movie.js
+++ src/store/movie.js
@@ -5,30 +5,39 @@ import _unionBy from "lodash/unionBy";
 export const movies = writable([]);
 export const loading = writable(false);
 export const theMovie = writable({});
+export const message = writable("Search for the movie title!");
 
 export const searchMovies = async (payload) => {
   if (get(loading)) return;
   loading.set(true);
 
-  const { title, type, year, number } = payload;
-  const OMDB_API_KEY = "7035c60c";
+  let total = 0;
 
-  const res = await axios.get(
-    `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}`
-  );
+  try {
+    const res = await _fetchMovie({
+      ...payload,
+      page: 1,
+    });
 
-  const { Search, totalResults } = res.data;
+    const { Search, totalResults } = res.data;
+    movies.set(Search);
+    total = totalResults;
+  } catch (msg) {
+    movies.set([]);
+    message.set(msg);
+    loading.set(false);
+    return;
+  }
 
-  movies.set(Search);
-
-  const pageLength = Math.ceil(totalResults / 10);
+  const pageLength = Math.ceil(total / 10);
 
   if (pageLength > 1) {
     for (let page = 2; page <= pageLength; page++) {
       if (page > number / 10) break;
-      const res = await axios.get(
-        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-      );
+      const res = await _fetchMovie({
+        ...payload,
+        page,
+      });
       const { Search } = res.data;
       movies.update(($movies) => _unionBy($movies, Search, "imdbID"));
     }
@@ -41,12 +50,33 @@ export const seachMovieWithId = async (id) => {
   if (get(loading)) return;
   loading.set(true);
 
-  //
-  const OMDB_API_KEY = "7035c60c";
-  const res = await axios.get(
-    `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
-  );
+  const res = await _fetchMovie({ id });
   theMovie.set(res.data);
 
   loading.set(false);
+};
+
+const _fetchMovie = (payLoad) => {
+  const { title, type, year, page, id } = payLoad;
+  const OMDB_API_KEY = "7035c60c";
+
+  const url = id
+    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
+    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(url);
+      console.log(res.data);
+
+      if (res.data.Error) {
+        reject(res.data.Error);
+      }
+
+      resolve(res);
+    } catch (e) {
+      console.log(e.response.status);
+      reject(e.msaage);
+    }
+  });
 };
